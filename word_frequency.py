1502161cc9961c51b6768afe286b9bbf43a3b6d3
Jonathan Frederick <jrfreder@gmail.com>
2015-09-03 14:56:18 -0400

    Added import file, call for word_frequency

--- word_frequency.py
+++ word_frequency.py
@@ -1,4 +1,17 @@
-#text << sample.txt  ??
+import re
 
 def word_frequency(text):
+    #re.sub(r'^a-z||', '', "bippity bop'pity boo..."#normalize text (no punctuation, no capitals)
+    #start counting dictionary
+    #print top 20
+
     return {"hello": 1}
+
+def main():
+    txt = open('sample.txt')#import file to string
+
+    word_frequency(txt)
+
+
+if __name__ == '__main__':
+    main()
