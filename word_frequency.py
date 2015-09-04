e92e9ea3f7620ce7eaa0e2051ad6db993b3a31d2
Jonathan Frederick <jrfreder@gmail.com>
2015-09-03 20:09:32 -0400

    Normal Mode completed

--- word_frequency.py
+++ word_frequency.py
@@ -2,27 +2,37 @@ import re
 
 def word_frequency(text):
     count_dict = {}
-    for line in text:
-        line = re.sub(r'[^a-z\s(\w\'\w)]', '', line.lower())#normalize text (no punctuation, no capitals)
-        for word in line.split():  #start counting dictionary
-            if word in count_dict:
-                count_dict[word] = count_dict[word]+1
-            else:
-                count_dict[word] = 1
+    text = re.sub(r'[^a-z\s(\w\'\w)]+', '', text.lower()).split()#normalize text (no punctuation, no capitals)
+#        print(line)
+    for word in text:  #start counting dictionary
+        if word in count_dict:
+            count_dict[word] = count_dict[word]+1
+        else:
+            count_dict[word] = 1
 
-    print(count_dict["the"])
-#        print(new_line)
+#    print(count_dict)
 
-
-    #print top 20
 #    for line in text:
 #        print(line)
-    return {"hello": 1}
+    return count_dict
 
 def main():
     txt = open('sample.txt')#import file to string
+    file_count = {}
+    for line in txt:
+        count = word_frequency(line)
+#        print(count)
+        for word in count:
+            if word in file_count:
+                file_count[word] = file_count[word]+count[word]
+            else:
+                file_count[word] = 1
+#            file_count[word] = file_count[word] + line[word]
+    #print(file_count)
 
-    word_frequency(txt)
+    top_20 = list(zip(range(1,21),sorted(file_count, key=lambda d: file_count[d] , reverse=True)))#print top 20
+    for w in top_20:
+        print(str(w[0]).rjust(3), "-", w[1].ljust(8), "-", file_count[w[1]])
 
 
 if __name__ == '__main__':
